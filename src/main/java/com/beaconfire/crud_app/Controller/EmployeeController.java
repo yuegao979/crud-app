df40bd62e940722671dd91be47b7375083b01e2e
yuegao <yueg6417@gmail.com>
2026-04-16 08:23:10 -0400

    address cors issue

--- src/main/java/com/beaconfire/crud_app/Controller/EmployeeController.java
+++ src/main/java/com/beaconfire/crud_app/Controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @Slf4j
 @RestController
 @RequestMapping("/employees")
